dbf98f166e836b751c436f696b7a96b66190c5a0
howardbdev <howarddevennish@gmail.com>
2020-02-26 12:01:59 -0500

    add show action

--- app/controllers/sushis_controller.rb
+++ app/controllers/sushis_controller.rb
@@ -2,6 +2,10 @@ class SushisController < ApplicationController
   def index
     # what needs to happen here??
     @sushis = Sushi.all
-  end  
+  end
+
+  def show
+    @sushi = Sushi.find(params[:id])
+  end
 
 end
