91942b6a31f42dc5cc907b73398bf37d3a521938
howardbdev <howarddevennish@gmail.com>
2020-02-26 11:04:08 -0500

    clean out code

--- app/controllers/sushis_controller.rb
+++
@@ -1,4 +0,0 @@
-class SushisController < ApplicationController
-
-
-end
--- db/migrate/20180115025443_create_sushis.rb
+++
@@ -1,8 +0,0 @@
-class CreateSushis < ActiveRecord::Migration[5.1]
-  def change
-    create_table :sushis do |t|
-      t.string :name
-      t.timestamps
-    end
-  end
-end
