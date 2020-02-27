114f7085cd985bf8a74ebc026d654cacad0db2e8
howardbdev <howarddevennish@gmail.com>
2020-02-27 14:44:45 -0500

    finish sushi controller actions

--- app/controllers/sushis_controller.rb
+++ app/controllers/sushis_controller.rb
@@ -8,4 +8,51 @@ class SushisController < ApplicationController
     @sushi = Sushi.find(params[:id])
   end
 
+  def new
+    @sushi = Sushi.new
+  end
+
+  def create
+    # receive form data to create a new sushi roll
+    @sushi = Sushi.new(sushi_params)
+
+    if @sushi.save
+      # success
+      redirect_to sushi_path(@sushi)
+    else
+      # failure - something went wrong...
+      render :new
+    end
+  end
+
+  def edit
+    @sushi = Sushi.find(params[:id])
+  end
+
+  def update
+    # receive form data to edit an existing sushi roll
+    @sushi = Sushi.find(params[:id])
+
+
+    if @sushi.update(sushi_params)
+      # success
+      redirect_to sushi_path(@sushi)
+    else
+      # failure - something went wrong...
+      render :edit
+    end
+  end
+
+  def destroy
+    @sushi = Sushi.find(params[:id])
+    @sushi.destroy
+    redirect_to '/'
+  end
+
+  private
+
+  def sushi_params
+    params.require(:sushi).permit(:name)
+  end
+
 end
