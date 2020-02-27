cc62ba69655f6905c703d36ce4b10cb608a7d718
howardbdev <howarddevennish@gmail.com>
2020-02-27 14:45:32 -0500

    add the rest of the RESTful routes

--- config/routes.rb
+++ config/routes.rb
@@ -5,11 +5,12 @@ Rails.application.routes.draw do
   get '/', to: "sushis#home"
 
   get '/sushis', to: "sushis#index"
+  get '/sushis/new', to: "sushis#new"
   get '/sushis/:id', to: "sushis#show", as: "sushi"
-
-
-
-
+  post '/sushis', to: "sushis#create"
+  get '/sushis/:id/edit', to: "sushis#edit", as: "edit_sushi"
+  patch '/sushis/:id', to: "sushis#update"
+  delete '/sushis/:id', to: "sushis#destroy"
 
   # MUST BE AT THE BOTTOM!!!!
   get '/:anything', to: "application#wrong_page"
