989f661f0cad75cf67b5a417ad021f3e0f91e1a1
howardbdev <howarddevennish@gmail.com>
2020-03-03 13:22:32 -0500

    comment and add some validators

--- app/models/sushi.rb
+++ app/models/sushi.rb
@@ -1,4 +1,9 @@
 class Sushi < ApplicationRecord
   validates :name, presence: true
+  validates :name, length: { minimum: 2 }
   validates :name, uniqueness: true
+
+  # validates(:name { presence: true, uniqueness: true })
+
+  # validates_presence_of :name
 end
